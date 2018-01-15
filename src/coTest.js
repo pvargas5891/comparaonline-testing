787b9c149cda2ac74fa2a1248fbbe3e798a98f78
Pablo Vargas <pvargas.figueroa@gmail.com>
2018-01-15 16:03:28 -0300

    add rule for Super Sale and refactoring code

--- src/coTest.js
+++ src/coTest.js
@@ -5,12 +5,21 @@ class Product {
     this.price = price;
   }
 }
-
+/**
+ * Class CarInsurance
+ * Description evaluation class for coverage in insurance car
+ */
 class CarInsurance {
   constructor(products = []) {
     this.products = products;
   }
-  updatePrice() {
+
+  /**
+   * Function evaluationCoveragePrice
+   * Description evalaution for price in all coverage
+   */
+  evaluationCoveragePrice(){
+
     for (var i = 0; i < this.products.length; i++) {
       if (this.products[i].name != 'Full Coverage' && this.products[i].name != 'Special Full Coverage') {
         if (this.products[i].price > 0) {
@@ -35,19 +44,44 @@ class CarInsurance {
           }
         }
       }
+    }
+
+  }
+  
+  /**
+   * Function evaluationCoveragePrice
+   * Description evalaution for price in all coverage
+   */
+  evaluationCoverageSellIn(){
+
+    for (var i = 0; i < this.products.length; i++) {
       if (this.products[i].name != 'Mega Coverage') {
         this.products[i].sellIn = this.products[i].sellIn - 1;
       }
+
+
       if (this.products[i].sellIn < 0) {
         if (this.products[i].name != 'Full Coverage') {
           if (this.products[i].name != 'Special Full Coverage') {
             if (this.products[i].price > 0) {
               if (this.products[i].name != 'Mega Coverage') {
                 this.products[i].price = this.products[i].price - 1;
+                /**
+                * Add rule "Products Super Sale degrade in price twice as fast as normal products"
+                */
+                if (this.products[i].name == 'Super Sale'){
+                  this.products[i].price = this.products[i].price - 1;
+                }
               }
             }
           } else {
             this.products[i].price = this.products[i].price - this.products[i].price;
+            /**
+             * Add rule "Products Super Sale degrade in price twice as fast as normal products"
+             */
+            if (this.products[i].name == 'Super Sale'){
+                  this.products[i].price = this.products[i].price - this.products[i].price;
+            }
           }
         } else {
           if (this.products[i].price < 50) {
@@ -56,6 +90,19 @@ class CarInsurance {
         }
       }
     }
+
+  }  
+  
+  /**
+   * Function updatePrice
+   * Description execute evaluation for insurance car
+   */
+  updatePrice() {
+
+    
+      this.evaluationCoveragePrice();
+
+      this.evaluationCoverageSellIn();
 
     return this.products;
   }
